c52200ebe776a2620e1dbeaef798a7d87a284ad3
John Perri Cruz <johnperricruz@gmail.com>
2017-09-07 16:45:52 -0700

    Fixes : Fixed the navigation urls for user

--- resources/views/includes/user/sidebar.blade.php
+++ resources/views/includes/user/sidebar.blade.php
@@ -14,22 +14,22 @@
            </li>-->      
 		   <li><a <?php echo($openable == 'Leaves') ? 'class="active"' : '' ;?>  href="#"><span class="icon color9"><i class="fa fa-plane"></i></span>Leaves<span class="caret"></span></a>
                 <ul  <?php echo($openable == 'Leaves') ? 'style="display:block;"' : '' ;?>>
-                    <li><a <?php echo($page == 'Leaves') ? 'class="active"' : '' ;?> href="/user/leaves">View</a></li>
-					<li><a <?php echo($page == 'Request Leave') ? 'class="active"' : '' ;?>href="/user/request-leave">Request Leave</a></li>
+                    <li><a <?php echo($page == 'Leaves') ? 'class="active"' : '' ;?> href="<?php echo url('user/leaves'); ?>">View</a></li>
+					<li><a <?php echo($page == 'Request Leave') ? 'class="active"' : '' ;?>href="<?php echo url('user/request-leave'); ?>">Request Leave</a></li>
                 </ul>
             </li>
             <li><a <?php echo($openable == 'Overtime') ? 'class="active"' : '' ;?> href="#"><span class="icon color9"><i class="fa fa-clock-o"></i></span>Overtime<span class="caret"></span></a>
                 <ul  <?php echo($openable == 'Overtime') ? 'style="display:block;"' : '' ;?>>
-                    <li><a <?php echo($page == 'Overtime') ? 'class="active"' : '' ;?> href="/user/overtime">View</a></li>
-					<li><a href="/user/overtime-approval">Request Overtime</a></li>
+                    <li><a <?php echo($page == 'Overtime') ? 'class="active"' : '' ;?> href="<?php echo url('user/overtime'); ?>">View</a></li>
+					<li><a href="<?php echo url('user/overtime-approval'); ?>">Request Overtime</a></li>
                 </ul>
             </li>
         </ul>
         <ul class="sidebar-panel nav">
             <li class="sidetitle">MORE</li>
-			<li><a <?php echo($page == 'Files') ? 'class="active"' : '' ;?> href="/user/files"><span class="icon color15"><i class="fa fa-file-o"></i></span>Files</a></li>
-			<li><a href="/lock"><span class="icon color7"><i class="fa fa-lock"></i></span>Lock Account</a></li>
-		    <li><a href="/process/logout"><span class="icon color7"><i class="fa fa-sign-out"></i></span>Logout</a></li>
+			<li><a <?php echo($page == 'Files') ? 'class="active"' : '' ;?> href="<?php echo url('user/files'); ?>"><span class="icon color15"><i class="fa fa-file-o"></i></span>Files</a></li>
+			<li><a href="<?php echo url('lock'); ?>"><span class="icon color7"><i class="fa fa-lock"></i></span>Lock Account</a></li>
+		    <li><a href="<?php echo url('process/logout'); ?>"><span class="icon color7"><i class="fa fa-sign-out"></i></span>Logout</a></li>
         </ul>
     </div>
     <!-- END SIDEBAR -->
