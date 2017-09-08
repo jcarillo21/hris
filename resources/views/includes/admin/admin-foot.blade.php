3fc6a4fc9e3ca154c2326bafb98b2e7904b41185
John Perri Cruz <johnperricruz@gmail.com>
2017-09-08 10:32:41 -0700

    Improvement : copyright links

--- resources/views/includes/admin/admin-foot.blade.php
+++ resources/views/includes/admin/admin-foot.blade.php
@@ -1,7 +1,7 @@
 		<!-- Start Footer -->
 		<div class="row footer">
 			<div class="col-md-6 text-left">
-				Copyright © <?=date('Y')?> <a href="https://www.primeview.com" target="_blank">Primeview</a> All rights reserved.
+				Copyright © <?=date('Y')?> <a href="#" >CiceroHR</a> All rights reserved.
 			</div>
 		</div>
 		<!-- End Footer -->
